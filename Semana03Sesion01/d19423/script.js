c0a93089ae9d4d511f634c458d6ee36f4100e23f
Roberto Pineda <rpineda@x-codec.net>
2023-09-27 21:07:06 -0500

    clase 27092023 primera parte

---
+++ Semana03Sesion01/d19423/script.js
@@ -0,0 +1,23 @@
+/*console.log("Hola desde el script")
+console.info("Info desde el script")
+console.warn("Warning desde el script")
+console.error("Un error desde el script")*/
+
+var fecha_de_nacimiento;
+fecha_de_nacimiento = "1983-08-28"
+console.log(fecha_de_nacimiento);
+fecha_de_nacimiento = 123456;
+console.log(fecha_de_nacimiento);
+
+var uno, dos, tres;
+var cuatro = 1, cinco= 12, seis = 10;
+
+console.log(uno,dos,tres,cuatro,cinco,seis)
+const PI = 3.1416;
+console.log(PI);
+//PI = 3.15;
+//console.log(PI)
+let numero1 = 10
+let numero2 = 20
+let resultado = numero1+ numero2;
+console.log(resultado)
