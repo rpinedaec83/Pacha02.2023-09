4f7cae9506114e8bb622f7012974a0cb8830099d
Your Name <arangoalmonacin-1994@hotmail.com>
2023-09-29 22:06:34 -0500

    index

---
+++ Hackaton03/SV48305061/ejercicio01.js
@@ -0,0 +1,7 @@
+let nro=prompt("Escribe un numero de 3 digitos: ")
+if (nro<999 && nro >99) {
+    console.log("El numero sigue las reglas: "+numero)
+}
+else {
+    console.log("El numero no sigue las reglas: "+numero)
+}
