0598a4757e7821bf32d376b2729a1b4fd921616d
omar <omarprograma72@gmail.com>
2023-10-09 00:38:04 -0500

    hackaton04

--- Hackaton04/SV70380181.reto2/script10.js
+++ Hackaton04/SV70380181.reto2/script10.js
@@ -4,7 +4,6 @@ function objetoAMatriz(objeto)
     return Object.entries(objeto);
 }
   
-  // Ejemplo de uso:
   const miObjeto = { a: 1, b: 2 };
   const matrizResultado = objetoAMatriz(miObjeto);
   console.log(matrizResultado);
