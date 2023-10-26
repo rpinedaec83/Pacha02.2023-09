b594e0da751e67377343ddba17be86da61b576d7
Roberto Pineda <rpineda@x-codec.net>
2023-10-25 22:19:27 -0500

    Para quien quiera complicarse

--- Semana07Sesion02/d19423/index.js
+++ Semana07Sesion02/d19423/index.js
@@ -7,15 +7,17 @@ const http = require('http');
 const app = express();
 // Log requests to the console.
 app.use(logger('dev'));
+app.use(express.static(__dirname + "/public"));
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 // app.get('*', (req, res) => res.status(200).send({
 //      message: 'Bienvenido a mi api.',
 // }));
 
-app.get("/usuario", (req, res) => {
+app.get("/api/usuario", (req, res) => {
     if (arrUsuarios.length != 0)
         return res.status(200).send(arrUsuarios)
     else
@@ -26,7 +28,7 @@ app.get("/usuario", (req, res) => {
 
 let arrUsuarios = [];
 
-app.post("/usuario", (req, res) => {
+app.post("/api/usuario", (req, res) => {
     console.log(req.body);
     let objUsuario = req.body;
     if (objUsuario.usuario != undefined && objUsuario.password != undefined) {
@@ -42,7 +44,7 @@ app.post("/usuario", (req, res) => {
     }
 })
 
-app.put("/usuario", (req, res) => {
+app.put("/api/usuario", (req, res) => {
     res.status(200).send({
         message: "Recurso modificado correctamente"
     })
@@ -54,13 +56,13 @@ app.patch("/usuario", (req, res) => {
     })
 })
 
-app.delete("/usuario", (req, res) => {
+app.delete("/api/usuario", (req, res) => {
     res.status(200).send({
         message: "Recurso borrado correctamente"
     })
 })
 
-app.get("/clima", async (req, res) => {
+app.get("/api/clima", async (req, res) => {
     let ciudad = req.query.ciudad;
     let str = `http://api.weatherapi.com/v1/current.json?key=ec8a9f98a9e74f82bd613118233005&q=${ciudad}&aqi=no`;
     const response = await fetch(str)
