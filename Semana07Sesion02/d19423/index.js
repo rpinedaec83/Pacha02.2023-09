d5f6b455316b1c0ae6b56522f30e6cea7325999b
Roberto Pineda <rpineda@x-codec.net>
2023-10-25 21:37:46 -0500

    Clase 25102023 segundta parte

--- Semana07Sesion02/d19423/index.js
+++ Semana07Sesion02/d19423/index.js
@@ -1,6 +1,6 @@
-const express       = require('express');
-const logger        = require('morgan');
-const bodyParser    = require('body-parser');
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 // This will be our application entry. We'll setup our server here.
 const http = require('http');
 // Set up the express app
@@ -15,13 +15,63 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //      message: 'Bienvenido a mi api.',
 // }));
 
-app.get("/usuario",(req, res) => {
-    let usuario = req.query.user;
-    return res.status(200).send({
-        user: usuario,
-        password: "aun no..."
+app.get("/usuario", (req, res) => {
+    if (arrUsuarios.length != 0)
+        return res.status(200).send(arrUsuarios)
+    else
+        res.status(400).send({
+            message: "no hay data"
+        })
+})
+
+let arrUsuarios = [];
+
+app.post("/usuario", (req, res) => {
+    console.log(req.body);
+    let objUsuario = req.body;
+    if (objUsuario.usuario != undefined && objUsuario.password != undefined) {
+        arrUsuarios.push(objUsuario);
+        res.status(201).send({
+            message: "Recurso creado correctamente"
+        })
+    }
+    else {
+        res.status(401).send({
+            message: "Error, revisa tu programacion"
+        })
+    }
+})
+
+app.put("/usuario", (req, res) => {
+    res.status(200).send({
+        message: "Recurso modificado correctamente"
     })
 })
+
+app.patch("/usuario", (req, res) => {
+    res.status(200).send({
+        message: "Recurso modificado parcialmente"
+    })
+})
+
+app.delete("/usuario", (req, res) => {
+    res.status(200).send({
+        message: "Recurso borrado correctamente"
+    })
+})
+
+app.get("/clima", async (req, res) => {
+    let ciudad = req.query.ciudad;
+    let str = `http://api.weatherapi.com/v1/current.json?key=ec8a9f98a9e74f82bd613118233005&q=${ciudad}&aqi=no`;
+    const response = await fetch(str)
+    const jsonResponse = await response.json();
+    console.log(jsonResponse)
+    res.status(200).send({
+        jsonResponse
+    })
+})
+
+
 
 const port = parseInt(process.env.PORT, 10) || 8000;
 app.set('port', port);
