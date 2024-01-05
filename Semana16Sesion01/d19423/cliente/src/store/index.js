f5488dd1684633a8f8c2179bb3a94ad9a78b877d
Roberto Pineda <rpineda@x-codec.net>
2024-01-05 01:58:14 -0500

    Clase 03012024

---
+++ Semana16Sesion01/d19423/cliente/src/store/index.js
@@ -0,0 +1,41 @@
+import { createStore } from 'vuex';
+export default createStore({
+  state: {
+    token: null
+  },
+  mutations: {
+    setToken(state, payload) {
+      state.token = payload
+    }
+  },
+  actions: {
+    async login({ commit }, usuario) {
+      try {
+        const res = await fetch('http://localhost:8009/api/user/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(usuario)
+        })
+        const usuarioDB = await res.json()
+        console.log(usuarioDB.data.token)
+        commit('setToken', usuarioDB.data.token)
+        localStorage.setItem('token2', usuarioDB.data.token)
+      } catch (error) {
+        console.log('error: ', error)
+      }
+    },
+    obtenerToken({ commit }) {
+      if (localStorage.getItem('token')) {
+        commit('setToken', localStorage.getItem('token'))
+      } else {
+        commit('setToken', null)
+      }
+    },
+    cerrarSesion({ commit }) {
+      commit('setToken', null)
+      localStorage.removeItem('token2')
+  }
+  }
+})
